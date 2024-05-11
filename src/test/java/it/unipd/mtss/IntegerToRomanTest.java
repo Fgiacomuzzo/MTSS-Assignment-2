2316ac7143fb58931058cb0128e7a0f65083ef90
Francesco Giacomuzzo <giacomuzzofrancesco3@gmail.com>
2024-05-11 14:58:12 +0200

    fix classes

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,10 +1,25 @@
 package it.unipd.mtss;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class IntegerToRomanTest {
-    @Test
-    public void testConvert() {
+    static IntegerToRoman converter = null;
 
+    @BeforeClass
+    public static void initializeConverter() {
+        converter = new IntegerToRoman();
+    }
+
+    @Test(expected = NumberBelowZeroException.class)
+    public void testNumberUnderZero() throws NumberBelowZeroException, NumberAOneThousand {
+        int arabic_number = -1;
+        converter.convert(arabic_number);
+    }
+
+    @Test(expected = NumberAOneThousand.class)
+    public void testNumberAbove1000() throws NumberBelowZeroException, NumberAOneThousand {
+        int arabic_number = 1001;
+        converter.convert(arabic_number);
     }
 }
